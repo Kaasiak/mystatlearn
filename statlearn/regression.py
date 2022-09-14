5b11fae189e8174dfdc0367541d8349ce9943feb
Kaasiak <kasia.kobalczyk@gmail.com>
2022-09-14 17:05:53 +0200

    Add ridge regression

--- statlearn/regression.py
+++ statlearn/regression.py
@@ -2,12 +2,12 @@ import numpy as np
 
 class LinearResgression():
 
-    def __init__(self):
+    def __init__(self, penalty=None, C=None):
         self.train_X = None
         self.train_y = None
         self.beta = None
-        self.Q = None
-        self.R = None
+        self.penalty = penalty
+        self.C = C
     
     def fit(self, X, y, add_intercept=False):
         if add_intercept:
@@ -16,12 +16,19 @@ class LinearResgression():
             X = X.reshape(-1, 1)    
         self.train_X = X
         self.train_y = y
-        self.Q, self.R = np.linalg.qr(X)
-        # self.beta = np.linalg.inv(X.T @ X) @ X.T @ y
-        self.beta = np.linalg.inv(self.R) @ self.Q.T @ y
+        if self.penalty is None:
+            self.Q, self.R = np.linalg.qr(X)
+            self.beta = np.linalg.inv(self.R) @ self.Q.T @ y
+        if self.penalty == 'L2':
+            self.beta = np.linalg.inv(
+                X.T @ X + self.C * np.eye(X.shape[1])) @ X.T @ y
 
     def transform(self):
-        return self.Q @ self.Q.T @ self.train_y
+        if self.penalty is None:
+            y_hat = self.Q @ self.Q.T @ self.train_y
+        else:
+            y_hat = self.X @ self.beta
+        return y_hat
     
     def predict(self, X, add_intercept=False):
         if add_intercept:
